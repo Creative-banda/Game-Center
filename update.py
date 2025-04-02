396cfe882c699c9b6bd2b187986288784a22f57a
Creative-banda <4kpuspa@gmail.com>
2025-04-02 18:12:05 +0530

    Add restart functionality to update.py

--- update.py
+++ update.py
@@ -3,8 +3,11 @@ import sys, subprocess, pathlib
 
 current_path = pathlib.Path(__file__).parent.resolve()
 
-def restart_script(self):
+def restart_script():
     """Runs the script again and exits the current one."""    
+    print("Restarting script...")
     subprocess.Popen([sys.executable, f"python {current_path}/main.py"])
     sys.exit()  # Exit current script
 
+
+restart_script()
