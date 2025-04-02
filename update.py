4907068b3aa1f5fbbb94af4fbe612b2b5f3c7cb4
Mohd Ahtesham <117986678+Creative-banda@users.noreply.github.com>
2025-04-02 18:26:59 +0530

    Update update.py

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ current_path = pathlib.Path(__file__).parent.resolve()
 def restart_script():
     """Runs the script again and exits the current one."""    
     print("Restarting script...")
-    subprocess.Popen([sys.executable, f"python {current_path}/main.py"])
+    subprocess.Popen(["python", f"{current_path}/main.py"])
     sys.exit()  # Exit current script
 
 
