44d28597ade7110337291d755dad5de5661d8ce2
Mohd Ahtesham <117986678+Creative-banda@users.noreply.github.com>
2025-04-04 11:55:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -112,13 +112,10 @@ class GameApp(ctk.CTk):
         try:
             self.status_label.configure(text="Checking for updates...")
             result = subprocess.run(["git", "pull"], cwd=self.repo_path, capture_output=True, text=True)
-            print("GIT PULL RESULT", result.stdout)
 
             if "Already up to date." in result.stdout:
-                print("No updates available.")
                 self.status_label.configure(text="No updates available.")
             else:
-                print("Update successful:", result.stdout)
                 self.status_label.configure(text="Update successful. Restarting...")
 
                 # Close the current application, startup script will run the new version
@@ -321,7 +318,6 @@ class GameApp(ctk.CTk):
 
             if game_info:
                 game_path = game_info["path"]
-                print(game_path)
                 
                 if game_info["type"] == "python":
                     os.system(f"python {game_path}")  # Run Pygame game
